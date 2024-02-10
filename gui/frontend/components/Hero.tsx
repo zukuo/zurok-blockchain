18aabe1a0e9cb58548455eba1ff381eaaa4a1e96
Eesa Ahmad <eesa.ahmad@student.manchester.ac.uk>
2024-02-10 22:36:26 +0000

    Fix UI aesthetics and add pagination

--- gui/frontend/components/Hero.tsx
+++ gui/frontend/components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { SiZcash } from "react-icons/si"
 
 const Hero = () => {
   return (
--- gui/frontend/components/Wallet.tsx
+++ gui/frontend/components/Wallet.tsx
@@ -1,13 +1,25 @@
-import React, {useEffect, useState} from "react";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from "@nextui-org/react";
+import React, {useEffect, useMemo, useState} from "react"
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  Spinner,
+  Pagination
+} from "@nextui-org/react";
 import {GetAddressesWithBalances} from "../wailsjs/wailsjs/go/gui/App"
+import {Button} from "@nextui-org/button"
+import { FaPlus } from "react-icons/fa";
 
 const Wallet = () => {
   const node = "3000"
   let addressBalances: { key: number; address: string; balance: number; }[]  = []
 
+  // Get JSON of key, address, balance
   const [wallets, setWallets] = useState(addressBalances)
-
   useEffect(() => {
     const fetchWallets = () => {
       try {
@@ -25,23 +37,63 @@ const Wallet = () => {
     fetchWallets()
   }, [])
 
+  // Check if it is loading data
+  const loadingState =  wallets?.length === 0 ? "loading" : "idle"
+
+  // Table Pagination
+  const [page, setPage] = useState(1);
+  const rowsPerPage = 10;
+  const pages = Math.ceil(wallets.length / rowsPerPage);
+  const items = useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+    return wallets.slice(start, end);
+  }, [page, wallets]);
+
   return (
-    <div className="flex justify-center">
-      <Table aria-label="Wallet Addresses with Balances" className="w-2/3 text-gray-300">
-        <TableHeader>
-          <TableColumn key="address">Address</TableColumn>
-          <TableColumn key="balance" align={"center"} width={150}>Balance</TableColumn>
-        </TableHeader>
-        <TableBody items={wallets}>
-          {(item) => (
-            <TableRow key={item.key}>
-              {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
-            </TableRow>
-          )}
-        </TableBody>
-        {/*<TableBody emptyContent={"No wallets found."}>{[]}</TableBody>*/}
-      </Table>
-    </div>
+    <>
+      <div className="flex justify-center">
+        <Table aria-label="Wallet Addresses with Balances"
+               className="w-2/5 text-gray-300 font-mono"
+               color={"success"}
+               bottomContent={pages > 1 ?
+                 <div className="flex w-full justify-center">
+                   <Pagination
+                     isCompact
+                     showControls
+                     showShadow
+                     loop
+                     color="success"
+                     initialPage={1}
+                     page={page}
+                     total={pages}
+                     onChange={(page) => setPage(page)}
+                   />
+                 </div> : null
+               }
+               classNames={{
+                 wrapper: "max-h-[490px]",
+               }}>
+          <TableHeader>
+            <TableColumn key="address">Address</TableColumn>
+            <TableColumn key="balance" align={"center"} width={150} className="text-center">Balance</TableColumn>
+          </TableHeader>
+          <TableBody items={items} loadingContent={<Spinner/>} loadingState={loadingState}>
+            {(item) => (
+              <TableRow key={item.key}>
+                {(columnKey) => <TableCell className="m-auto text-center">{getKeyValue(item, columnKey)}</TableCell>}
+              </TableRow>
+            )}
+          </TableBody>
+          {/*<TableBody emptyContent={"No wallets found."}>{[]}</TableBody>*/}
+        </Table>
+      </div>
+      <div className="flex justify-center p-7">
+        <Button endContent={<FaPlus/>} radius="lg" color={"success"} className="text-black shadow-lg">
+          New Wallet
+        </Button>
+      </div>
+    </>
   )
 }
 
